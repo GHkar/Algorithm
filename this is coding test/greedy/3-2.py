a6d9deb8d6ba3eec465b7e740adedb2aafcff120
KimAeri <52875499+GHkar@users.noreply.github.com>
2022-06-02 12:02:11 +0900

    큰 수의 법칙

---
+++ this is coding test/greedy/3-2.py
@@ -0,0 +1,18 @@
+# 큰 수의 법칙
+
+n,m,k = map(int, input().split())
+
+data = list(map(int, input().split()))
+
+data.sort(reverse = True)
+first = data[0]
+second = data[1]
+
+count = int(m / (k+1)) * k
+count += m % (k+1)
+
+result = 0
+result += first * count
+result += second * (m-count)
+
+print(result)
