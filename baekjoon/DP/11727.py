4d80b761954b40c27062045bc2e297ecde1b7edb
KimAeri <52875499+GHkar@users.noreply.github.com>
2022-07-28 10:43:23 +0900

    2xn 타일링2

---
+++ baekjoon/DP/11727.py
@@ -0,0 +1,15 @@
+# 2xn 타일링 2
+
+# 점화식 n = 2(n-2) + (n-1)
+
+n = int(input())
+
+dp = [0] * 1001
+
+dp[1] = 1
+dp[2] = 3
+
+for i in range(3, n+1) :
+    dp[i] = 2 * dp[i-2] + dp[i-1]
+
+print(dp[n] % 10007)
